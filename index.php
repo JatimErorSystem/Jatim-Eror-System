17bf98eb707cc57a14e2099898870840271881cd
Jatim Eror System <129208858+JatimErorSystem@users.noreply.github.com>
2023-03-29 00:03:33 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<head>
+<title>FAKE SITE 666</title>
+<link href="https://fonts.googleapis.com/css?family=Inconsolata&display=swap" rel="stylesheet">
+<meta charset="UTF-8">
+<meta name="description" content="FakeSite666">
+<meta name="keywords" content="FakeSite666">
+<meta http-equiv="cache-control" content="index,cache">
+<meta http-equiv="pragma" content="index,cache">
+<meta name="theme-color" content="black">
+</head>
+<center>
+<body bgcolor="black">
+<div style="height: auto; min-height: 100%;">
+<div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;">
+<b>
+<img src="https://i.ibb.co/G3C2tcN/20230326-171023.jpg" width="280" height="280">
+</font>
+<font face="Inconsolata" size="5" color="white"><br><i>SAYA TIDAK BUTUH PERJUANGAN ATAUPUN MERDEKA, PERCUMA MERDEKA<br>KALAU RAKYAT MASIH SENGSARA DIATAS PENDERITAAN PEMERINTAH<br><br>BIG FAMILLY JATIM EROR SYSTEM<br><font color="red">(QYURA666|FAKESITE66|TIANDEV666)<br>
+ 
+<br><br><br><br>
